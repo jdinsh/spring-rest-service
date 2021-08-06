117a07247b14828b1e5e68de88f01de92e52cae6
jdinsh <jdinsh@gmail.com>
2021-08-05 22:46:12 -0400

    Created a user
    This session - we are created a user and returned back the created user id url using  ServletUriComponentsBuilder.fromCurrentRequest()

--- src/main/java/com/djlearning/springrestservice/user/UserResource.java
+++ src/main/java/com/djlearning/springrestservice/user/UserResource.java
@@ -1,10 +1,12 @@
 package com.djlearning.springrestservice.user;
 
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,13 +16,30 @@ public class UserResource {
     private  UserDaoService  userDaoService;
     //retreiveAllUsers
 
+    /**
+     *
+     * @return all users
+     */
     @GetMapping("/users")
     public List<User> getAllUsers(){
         return userDaoService.findAll();
     }
 
+    /**
+     *
+     * @return user
+     */
     @GetMapping("/users/{id}")
     public  User getUserById(@PathVariable int id){
         return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public  ResponseEntity<Object> createUser(@RequestBody User user){
+        //ServletUriComponentsBuilder
+        User savedUser =  userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
